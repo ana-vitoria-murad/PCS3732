ef302cddbf877ab978db67fa397ad32d510a92b3
Yasmin Francisquetti <132413424+yasminfrancisquetti@users.noreply.github.com>
2026-07-15 15:43:27 -0300

    Add LED control to the metronome script

--- ativ7/desafio.py
+++ ativ7/desafio.py
@@ -3,6 +3,7 @@ import time
 
 SERVO_PIN = 18
 BUZZER_PIN = 4
+LED_PIN = 17
 
 BTN_MAIS_10 = 26
 BTN_MENOS_10 = 20
@@ -16,6 +17,8 @@ pi = pigpio.pi()
 if not pi.connected:
     print("Erro: Não foi possível conectar ao daemon do pigpio.")
     exit()
+
+pi.set_mode(LED_PIN, pigpio.OUTPUT)
 
 bpm = 60.0
 
@@ -43,11 +46,10 @@ botoes = [BTN_MAIS_10, BTN_MENOS_10, BTN_MAIS_1, BTN_MENOS_1]
 for btn in botoes:
     pi.set_mode(btn, pigpio.INPUT)
     pi.set_pull_up_down(btn, pigpio.PUD_UP)
-    
-    pi.set_glitch_filter(btn, 200000)
-    
-    pi.callback(btn, pigpio.FALLING_EDGE, altera_bpm)
 
+    pi.set_glitch_filter(btn, 200000)
+
+    pi.callback(btn, pigpio.FALLING_EDGE, altera_bpm)
 
 estado = False
 print(f"Metrônomo iniciado a {bpm} BPM. Pressione Ctrl+C para sair.")
@@ -63,12 +65,15 @@ try:
 
         pi.set_PWM_frequency(BUZZER_PIN, FREQ_A5)
         pi.set_PWM_dutycycle(BUZZER_PIN, 128)
-        
+        pi.write(LED_PIN, 1)
+
         time.sleep(0.10)
-        
+
         pi.set_PWM_dutycycle(BUZZER_PIN, 0)
-        estado = not estado
+        pi.write(LED_PIN, 0)
         
+        estado = not estado
+
         tempo_ciclo = 60.0 / bpm
         
         t_atual = time.time()
@@ -85,6 +90,7 @@ except KeyboardInterrupt:
 finally:
     pi.set_servo_pulsewidth(SERVO_PIN, 0)
     pi.set_PWM_dutycycle(BUZZER_PIN, 0)
+    pi.write(LED_PIN, 0)
     
     for btn in botoes:
         pi.set_glitch_filter(btn, 0)
