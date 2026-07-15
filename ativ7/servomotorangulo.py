4c5f53fe06962d30b5577656b6c404f7cd31bdf2
Yasmin Francisquetti <132413424+yasminfrancisquetti@users.noreply.github.com>
2026-07-15 14:26:28 -0300

    Switch servo control from AngularServo to pigpio
    Refactor servo control to use pigpio library for pulse width modulation.

--- ativ7/servomotorangulo.py
+++ ativ7/servomotorangulo.py
@@ -1,27 +1,34 @@
-from gpiozero import AngularServo
+import pigpio
 from time import sleep
 
 SERVO_PIN = 18
 
-servo = AngularServo(
-    SERVO_PIN,
-    min_angle=-90,
-    max_angle=90
-)
+pi = pigpio.pi()
 
-while True:
-    servo.angle = -90
-    sleep(2)
+if not pi.connected:
+    print("Erro: Não foi possível conectar ao daemon do pigpio.")
+    exit()
 
-    servo.angle = -45
-    sleep(2)
+try:
+    while True:
+        pi.set_servo_pulsewidth(SERVO_PIN, 500)
+        sleep(2)
 
-    servo.angle = 0
-    sleep(2)
+        pi.set_servo_pulsewidth(SERVO_PIN, 1000)
+        sleep(2)
 
-    servo.angle = 45
-    sleep(2)
+        pi.set_servo_pulsewidth(SERVO_PIN, 1500)
+        sleep(2)
 
-    servo.angle = 90
-    sleep(2)
+        pi.set_servo_pulsewidth(SERVO_PIN, 2000)
+        sleep(2)
 
+        pi.set_servo_pulsewidth(SERVO_PIN, 2500)
+        sleep(2)
+
+except KeyboardInterrupt:
+    print("\nPrograma interrompido pelo usuário.")
+
+finally:
+    pi.set_servo_pulsewidth(SERVO_PIN, 0)
+    pi.stop()
